2470b5f3c610825457e50d7ccdd91231e13c3878
huangjunyu <huangjunyump3@sina.com>
2016-06-24 17:47:10 +0800

    20160624

--- app/src/main/java/com/yiwucheguanjia/carmgr/account/LoginActivity.java
+++ app/src/main/java/com/yiwucheguanjia/carmgr/account/LoginActivity.java
@@ -4,7 +4,9 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.pm.PackageManager;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -14,40 +16,73 @@ import android.app.LoaderManager.LoaderCallbacks;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+
+import com.yiwucheguanjia.carmgr.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import com.yiwucheguanjia.carmgr.R;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends Activity implements View.OnClickListener,LoaderCallbacks<Cursor> {
+
+    private String login_account;
+    private String login_password;
+
+    private Button email_sign_in_button;
+    private EditText login_account_edit;
+    private EditText login_password_edit;
+    private OkHttpClient client;
+    private static final MediaType JSON = MediaType.parse("application/json;charset:utf-8");
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+
     }
-//
+    private void initView(){
+        login_account_edit = (EditText)findViewById(R.id.login_account_edit);
+        login_password_edit = (EditText)findViewById(R.id.login_password_edit);
+        client = new OkHttpClient();
+    }
+    /*OkHttpClient client = new OkHttpClient();
+    String post(String url, String json) throws IOException {
+
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("platform", "android")
+                .add("name", "bug")
+                .add("subject", "XXXXXXXXXXXXXXX")
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (response.isSuccessful()) {
+            return response.body().string();
+        } else {
+            throw new IOException("Unexpected code " + response);
+        }
+    }*/
+
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         return null;
@@ -62,5 +97,27 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     public void onLoaderReset(Loader<Cursor> loader) {
 
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login_button:
+                login_account = login_account_edit.getText().toString();
+                login_password = login_password_edit.getText().toString();
+        }
+    }
+
+    /**
+     * Use an AsyncTask to fetch the user's email addresses on a background thread, and update
+     * the email text field with results on the main UI thread.
+     */
+    class SetupEmailAutoCompleteTask extends AsyncTask<Void, Void, List<String>> {
+        @Override
+        protected List<String> doInBackground(Void... params) {
+//            final Request request = new Request.Builder().post()
+            return null;
+        }
+    }
+
 }
 
